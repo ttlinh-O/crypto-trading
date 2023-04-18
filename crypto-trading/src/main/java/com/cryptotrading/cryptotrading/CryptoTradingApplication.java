57127658b8ef73a6020425eb062aebbead332ccc
ttlinh <totuanlinh@gmail.com>
2023-04-18 21:48:00 +0700

    initial commit

---
+++ crypto-trading/src/main/java/com/cryptotrading/cryptotrading/CryptoTradingApplication.java
@@ -0,0 +1,13 @@
+package com.cryptotrading.cryptotrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoTradingApplication.class, args);
+    }
+
+}
---
+++ crypto-trading/src/test/java/com/cryptotrading/cryptotrading/CryptoTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptotrading.cryptotrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
