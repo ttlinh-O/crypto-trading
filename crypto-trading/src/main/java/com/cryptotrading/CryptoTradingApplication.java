3cd078826715553c096db7b25ae0e6654b98f872
ttlinh <totuanlinh@gmail.com>
2023-04-18 22:07:23 +0700

    Add loombok, h2 and mapstruct dependencies

--- crypto-trading/src/main/java/com/cryptotrading/CryptoTradingApplication.java
+++ crypto-trading/src/main/java/com/cryptotrading/CryptoTradingApplication.java
@@ -1,4 +1,4 @@
-package com.cryptotrading.cryptotrading;
+package com.cryptotrading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- crypto-trading/src/test/java/com/cryptotrading/CryptoTradingApplicationTests.java
+++ crypto-trading/src/test/java/com/cryptotrading/CryptoTradingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cryptotrading.cryptotrading;
+package com.cryptotrading;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
